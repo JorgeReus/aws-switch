03e053f5d89ea8f92e8caac0ab54658c689ff1c0
Jorge Reus <j-g1996@live.com>
2020-09-28 21:18:50 -0500

    Initial commit

---
+++ src/go.mod
@@ -0,0 +1,8 @@
+module aws-switch
+
+go 1.15
+
+require (
+	github.com/bigkevmcd/go-configparser v0.0.0-20200217161103-d137835d2579
+	github.com/manifoldco/promptui v0.8.0
+)
---
+++ src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/bigkevmcd/go-configparser"
+	"github.com/manifoldco/promptui"
+	"io/ioutil"
+	"os/user"
+	"runtime"
+)
+
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	user, err := user.Current()
+	checkError(err)
+	credsPtr := flag.String("creds", fmt.Sprintf("%s/.aws/credentials", user.HomeDir), "AWS credentials file location")
+
+	var tmpfilePtr *string
+	if runtime.GOOS == "windows" {
+		tmpfilePtr = flag.String("tmp", "/AppData/Local/Temp/awsprofileswitcher.profile", "Temporary file location windows")
+	} else {
+		tmpfilePtr = flag.String("tmp", "/tmp/awsprofileswitcher.profile", "Temporary file location linux")
+	}
+
+	// tmpfilePtr := flag.String("tmp", "/tmp/awsprofileswitcher.profile", "Temporary file location")
+	flag.Parse()
+
+	// Parse the aws credentials
+	p, err := configparser.NewConfigParserFromFile(*credsPtr)
+	checkError(err)
+	sections := p.Sections()
+
+	// Display the menu
+	prompt := promptui.Select{
+		Label: "Select AWS profile",
+		Items: sections,
+	}
+	_, result, err := prompt.Run()
+	checkError(err)
+
+	// Return the selected option
+	data := []byte(fmt.Sprintf("export AWS_PROFILE=%s", result))
+	err = ioutil.WriteFile(*tmpfilePtr, data, 0644)
+	checkError(err)
+}
